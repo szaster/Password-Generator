f4ce181db09492082dc3d665ef50920224101be1
Svitlana Zaster <szaster@protonmail.com>
2021-08-21 17:21:28 -0500

    work in progress

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import "./App.css";
 import Options from "./components/Options";
 import NewPasswordButton from "./components/generateButton";
 import { Grid, Button, useMediaQuery, Hidden } from "@material-ui/core";
+import { ContactSupportOutlined } from "@material-ui/icons";
 
 function App() {
+  const [language, setLanguage] = React.useState("python");
+
   function handleStartClick() {
     alert("Start Button was clicked");
   }
@@ -14,7 +17,8 @@ function App() {
         Welcome! This application generates a random password based on the
         criteria provided by a user
       </h1>
-
+      <Button onClick={() => setLanguage("JavaScript")}>Change language</Button>
+      <p>Iam learning {language}</p>
       <Button style={{ background: "lime" }} onClick={handleStartClick}>
         START
       </Button>
