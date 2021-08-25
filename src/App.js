fd28550c7e2847e274e594494d56253b00ffde4b
Svitlana Zaster <szaster@protonmail.com>
2021-08-24 19:21:49 -0500

    Add a bit of style

--- src/App.js
+++ src/App.js
@@ -2,46 +2,42 @@ import React from "react";
 import "./App.css";
 import Options from "./components/Options";
 import NewPasswordButton from "./components/generateButton";
-import { Grid, Button, useMediaQuery, Hidden } from "@material-ui/core";
-import { ContactSupportOutlined } from "@material-ui/icons";
+import {
+  Grid,
+  Button,
+  useMediaQuery,
+  Hidden,
+  Paper,
+  Container,
+  Typography,
+} from "@material-ui/core";
 
 const endpoint = "https://api.github.com/users/szaster";
 
 function App() {
-  const [user, setUser] = React.useState(null);
-
-  React.useEffect(() => {
-    async function getUser() {
-      const response = await fetch(endpoint);
-      const data = await response.json();
-      setUser(data);
-    }
-    getUser();
-    // fetch(endpoint)
-    //   .then((response) => response.json())
-    //   .then((data) => setUser(data));
-  }, []);
   function handleStartClick() {
     alert("Start Button was clicked");
   }
-  return user ? (
+  return (
     <div className="App">
-      <h1 style={{ background: "pink" }}>
+      {/* <h1 style={{ background: "pink" }}>
         Welcome! This application generates a random password based on the
         criteria provided by a user
       </h1>
 
       <Button style={{ background: "lime" }} onClick={handleStartClick}>
         START
-      </Button>
-
-      <h2>{user.name}</h2>
-      <img alt="avatar" src={user.avatar_url} style={{ height: 50 }} />
-      <Options />
-      <NewPasswordButton />
+      </Button> */}
+      <Container maxWidth="sm" m={2}>
+        <Paper elevation={3} outlined square m={2}>
+          <Typography variant="h3" component="h3">
+            Options
+          </Typography>
+          <Options />
+        </Paper>
+        <NewPasswordButton />
+      </Container>
     </div>
-  ) : (
-    <p>Loading</p>
   );
 }
 
--- src/components/generateButton.js
+++ src/components/generateButton.js
@@ -3,7 +3,11 @@ import Grid, { Button } from "@material-ui/core";
 import Icons from "@material-ui/icons";
 
 const NewPasswordButton = () => {
-  return <Button style={{ background: "pink" }}>Generate Password</Button>;
+  return (
+    <Button style={{ background: "pink", margin: 20 }}>
+      Generate Password
+    </Button>
+  );
 };
 
 export default NewPasswordButton;
