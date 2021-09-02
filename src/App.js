55423a7835bf4ff030d848da53cd954bf1ddcd88
Svitlana Zaster <szaster@protonmail.com>
2021-09-02 17:27:50 -0500

    Working on the grid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Options from "./components/Options";
+import Header from "./components/Header";
 import NewPasswordButton from "./components/generateButton";
 import {
   Grid,
@@ -28,6 +29,7 @@ function App() {
       <Button style={{ background: "lime" }} onClick={handleStartClick}>
         START
       </Button> */}
+      <Header />
       <Container maxWidth="sm" m={2}>
         <Paper elevation={3} outlined square m={2}>
           {/* <Typography variant="h5" component="h5">
