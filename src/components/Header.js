8be1a374c20df16caffb2a462d126c07a42eed3f
Svitlana Zaster <szaster@protonmail.com>
2021-09-06 21:18:58 -0500

    restyled header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,25 @@
 import React from "react";
+import LockIcon from "@material-ui/icons/Lock";
+import { AppBar, Typography, makeStyles, Toolbar } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    marginLeft: theme.spacing(2),
+  },
+}));
 
 function Header() {
+  const classes = useStyles();
+
   return (
-    <>
-      <h3 style={{ background: "pink" }}>
-        Welcome! This application generates a random password based on the
-        criteria provided by a user
-      </h3>
-    </>
+    <AppBar color="secondary" position="fixed">
+      <Toolbar>
+        <LockIcon />
+        <Typography className={classes.title} variant="h6" component="h1">
+          Random Password Generator
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
 
