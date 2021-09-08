823a2714851faa7ef7e0efbe99cac8a10e01158a
Svitlana Zaster <szaster@protonmail.com>
2021-09-08 15:47:00 -0500

    added theme file

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { CssBaseline, MuiThemeProvider } from "@material-ui/core";
+import theme from "./theme";
 
 ReactDOM.render(
-  // <React.StrictMode>
-  <App />,
-  // </React.StrictMode>,
+  <MuiThemeProvider theme={theme}>
+    <CssBaseline />k
+    <App />
+  </MuiThemeProvider>,
   document.getElementById("root")
 );
