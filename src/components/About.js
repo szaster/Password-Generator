400f99a6326642e8cab2cb34355cb953d3467ac8
Svitlana Zaster <szaster@protonmail.com>
2021-10-04 10:43:49 -0500

    set up about page container

--- src/components/About.js
+++ src/components/About.js
@@ -8,47 +8,47 @@ import {
   Button,
   Box,
   Paper,
+  Container,
 } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
-  title: {
-    marginLeft: theme.spacing(2),
-  },
-  layout: {
+  root: { display: "flex", padding: "0px 70px" },
+  rowLayout: {
     display: "flex",
-    flexGrow: 1,
-    justifyContent: "space-between",
+    alignItems: "baseline",
   },
+  left: { marginRight: "auto" }, //aligns Flexboxes to the left!
 }));
 
-// function Header() {
 // const location = useLocation();
 
 function About() {
   const classes = useStyles();
   return (
-    <div>
-      <Paper
-        elevation={10}
-        outlined
-        square
-        m={20}
-        style={{ background: "#eceff1" }}
-      >
-        <Typography component="div" variant="subtitle1" gutterBottom>
-          <Box
-            borderBottom={1}
-            paddingBottom={2}
-            textAlign="center"
-            fontWeight="fontWeightMedium"
-            fontSize={20}
-            m={1}
-          >
-            About egirwgperk gpok r gop w select options for generating password
-          </Box>
-        </Typography>
-      </Paper>
+    <div className={classes.root}>
+      <Container style={{ paddingTop: 90 }} maxWidth="sm" m={10}>
+        <Paper
+          elevation={10}
+          outlined
+          square
+          m={50}
+          style={{ background: "#eceff1" }}
+        >
+          <Typography component="div" variant="subtitle1" gutterBottom>
+            <Box
+              paddingBottom={2}
+              textAlign="center"
+              fontWeight="fontWeightMedium"
+              fontSize={30}
+              m={1}
+            >
+              About egirwgperk gpok r gop w select options for generating
+              passwordbh rhb etgertg
+            </Box>
+          </Typography>
+        </Paper>
+      </Container>
     </div>
   );
 }
