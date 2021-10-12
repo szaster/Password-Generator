ceb5ad1ed40f10c56b3f4442a5365e1a733b0de2
Svitlana Zaster <szaster@protonmail.com>
2021-10-11 21:07:17 -0500

    Theme: work in progress

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { StyledEngineProvider } from "@mui/material/styles";
 import App from "./App";
+
 import CssBaseline from "@material-ui/core/CssBaseline";
 
 ReactDOM.render(
   <Router>
-    {/* <ThemeProvider theme={theme}> */}
-    <CssBaseline />
-    <App />
+    <StyledEngineProvider injectFirst>
+      {/* <ThemeProvider theme={theme}> */}
+      <CssBaseline />
+      <App />
+    </StyledEngineProvider>
     {/* </ThemeProvider> */}
   </Router>,
 
