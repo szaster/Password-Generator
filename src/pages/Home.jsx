cdd2a62efdcb56df464e1e80c2396c106e8f1fdb
Svitlana Zaster <szaster@protonmail.com>
2021-10-19 19:13:55 -0500

    wip

---
+++ src/pages/Home.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import Options from "./pages/components/Options";
+import { makeStyles } from "@mui/styles";
+import RegeneratePasswordButton from "./pages/components/RegenerateButton";
+import { Paper, Container } from "@mui/material";
+import { ThemeApp } from "./pages/components/BoxedThemeToggle";
+
+import { PasswordDisplay } from "./components/PasswordDisplay";
+
+export const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 250,
+    margin: "auto",
+    display: "flex",
+  },
+}));
+const initialOptions = {
+  lowercase: true,
+  uppercase: true,
+  specialCharacters: true,
+  numbers: true,
+  passwordLength: 10,
+};
+const initialPassword = generatePassword(initialOptions);
+function generatePasswordIndexes(passwordLength, max) {
+  const intMax = 2 ** 32 - 1;
+  const numbersArray = window.crypto.getRandomValues(
+    new Uint32Array(passwordLength)
+  );
+
+  const passwordIndexes = numbersArray.map((i) =>
+    Math.floor((max * i) / (intMax + 1))
+  );
+  return passwordIndexes;
+}
+function getCharacters(options) {
+  // password character set
+  const lowerAlphas = "qwertyuiopasdfghjklzxcvbnm";
+  const upperAlphas = lowerAlphas.toUpperCase();
+  const numbers = "0123456789";
+  const special = "!@#$%^&*()_+=-;:/?";
+
+  const allCharacters = [];
+  if (options.lowercase === true) {
+    allCharacters.push(lowerAlphas);
+  }
+  if (options.uppercase === true) {
+    allCharacters.push(upperAlphas);
+  }
+  if (options.specialCharacters === true) {
+    allCharacters.push(special);
+  }
+  if (options.numbers === true) {
+    allCharacters.push(numbers);
+  }
+
+  const charactersPool = allCharacters.join("");
+
+  return charactersPool;
+}
+function generatePassword(options) {
+  const charactersPool = getCharacters(options);
+  const indexes = generatePasswordIndexes(
+    options.passwordLength,
+    charactersPool.length
+  );
+  const chars = [];
+  indexes.forEach((idx) => chars.push(charactersPool[idx]));
+  console.log(chars);
+  const password = chars.join("");
+  return password;
+}
+export function Home() {
+  const [options, setOptions] = React.useState(initialOptions);
+  const [password, setPassword] = React.useState(initialPassword);
+
+  function handleNewOptions(newOptions) {
+    setOptions(newOptions);
+    const newPassword = generatePassword(newOptions);
+    setPassword(newPassword);
+  }
+
+  return (
+    <div className="App" style={{ paddingTop: 110 }}>
+      <ThemeApp />
+      <Container style={{ paddingTop: 110 }} maxWidth="sm" m={10}>
+        <Paper
+          elevation={10}
+          outlined
+          square
+          m={20}
+          style={{ background: "#eceff1" }}
+        >
+          <Options handleNewOptions={handleNewOptions} options={options} />
+          <PasswordDisplay
+            handleNewOptions={handleNewOptions}
+            password={password}
+          />
+        </Paper>
+        <RegeneratePasswordButton
+          handleNewOptions={handleNewOptions}
+          options={options}
+        />
+      </Container>
+    </div>
+  );
+}
---
+++ src/pages/components/PasswordDisplay.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Paper, Box, Typography } from "@mui/material";
+import { useStyles } from "../Home";
+
+export function PasswordDisplay(props) {
+  const password = props.password || "not set";
+  const classes = useStyles;
+  return (
+    <div>
+      <Paper style={{ background: "#b2dfdb" }} className={classes.root}>
+        <Typography component="div" variant="subtitle1" gutterBottom>
+          <Box
+            textAlign="center"
+            fontWeight="fontWeightLarge"
+            fontSize={25}
+            m={3}
+            paddingTop={2}
+            paddingRight={2}
+            paddingLeft={0}
+            paddingBottom={2}
+          >
+            {password}
+          </Box>
+        </Typography>
+      </Paper>
+    </div>
+  );
+}
